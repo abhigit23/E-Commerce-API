fa1ec02423f4d0806ac91fae177b96b7a74ab970
Abhijeet Bhandari <abhijeetbhandari84@gmail.com>
2023-05-27 00:47:10 +0530

    comment morgan package

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // rest of the packages
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
@@ -48,7 +48,7 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
